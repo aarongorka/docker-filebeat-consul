0c85aff0d3a43bbff738b6b69f5f857d54ab07d9
Aaron Gorka <aaron.gorka@domain.com.au>
2017-06-20 14:59:23 +1000

    Get Consul IP even if container name is different

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,7 @@ set -o pipefail
 shopt -s nullglob
 unalias -a
 
-curl "${NOMAD_IP_http}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d | tee /etc/filebeat/filebeat.yml
+IP="$(env | grep NOMAD_IP | head -n 1 | cut -f 2 -d '=')"
+
+curl "${IP}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d | tee /etc/filebeat/filebeat.yml
 filebeat -e -c /etc/filebeat/filebeat.yml
