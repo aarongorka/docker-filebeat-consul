02a36b1022c0ec3ffc2712eadd32ddd625621653
Aaron Gorka <aaron.gorka@domain.com.au>
2017-06-21 15:36:25 +1000

    filebeat executable not in PATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,4 @@ IP="$(env | grep NOMAD_IP | head -n 1 | cut -f 2 -d '=')"
 
 curl "${IP}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d | tee ./filebeat.yml
 
-filebeat -e -c ./filebeat.yml
+./filebeat -e -c ./filebeat.yml
