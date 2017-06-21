813f7117e782969aca6a0dfd5507779d453ceb99
Aaron Gorka <aaron.gorka@domain.com.au>
2017-06-21 13:40:54 +1000

    Update paths to filebeat.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,9 @@ set -o pipefail
 shopt -s nullglob
 unalias -a
 
+# there's multiple NOMAD_IP_x variables so just grab the value of one
 IP="$(env | grep NOMAD_IP | head -n 1 | cut -f 2 -d '=')"
 
-curl "${IP}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d | tee /etc/filebeat/filebeat.yml
-filebeat -e -c /etc/filebeat/filebeat.yml
+curl "${IP}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d | tee ./filebeat.yml
+
+filebeat -e -c ./filebeat.yml
