cd21b73d56678bc48106c7afdc9b913751b624fe
Aaron Gorka <aaron.gorka@domain.com.au>
2017-06-27 10:39:31 +1000

    Add fallback if filebeat.yml isn't available

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,12 @@ unalias -a
 # there's multiple NOMAD_IP_x variables so just grab the value of one
 IP="$(env | grep NOMAD_IP | head -n 1 | cut -f 2 -d '=')"
 
-curl "${IP}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d | tee ./filebeat.yml
+set +o errexit
+OUTPUT="$(curl -q "${IP}:8500/v1/kv/jobs/${NOMAD_META_name}/${NOMAD_META_version}/filebeat" | jq -r '.[] | .Value' | base64 -d)"
+set -o errexit
+
+if filebeat -configtest -e -c "<(${OUTPUT})"; then
+  echo "${OUTPUT}" | tee ./filebeat.yml
+fi
 
 ./filebeat -e -c ./filebeat.yml
